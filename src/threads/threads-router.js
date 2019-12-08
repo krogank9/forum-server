539602e63126fd6e836da9d30e09e3aff5852bee
krogank9 <wifi.mouse.app@gmail.com>
2019-12-08 01:35:09 -0500

    add error check when posting thread on invalid board

--- src/threads/threads-router.js
+++ src/threads/threads-router.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const xss = require('xss')
 const ThreadsService = require('./threads-service')
+const BoardsService = require('../boards/boards-service')
 const UsersRouter = require('../users/users-router')
 const { requireAuth } = require('../middleware/jwt-auth')
 
@@ -10,12 +11,12 @@ const jsonParser = express.json()
 
 const serializeThread = thread => ({
     id: thread.id,
-    name: xss(thread.name, {whiteList: []}),
+    name: xss(thread.name, { whiteList: [] }),
     author_id: thread.author_id,
     date_created: thread.date_created,
     board_id: thread.board_id,
     reply_count: thread.reply_count,
-    author_name: xss(thread.author_name, {whiteList: []}),
+    author_name: xss(thread.author_name, { whiteList: [] }),
     board_name: thread.board_name,
 })
 
@@ -32,32 +33,43 @@ threadsRouter.route('/')
         const { name, board_id, first_post_content } = req.body
         const newThread = { "name": name, "board_id": board_id, "author_id": req.user.id }
 
-        for (const [key, value] of Object.entries({...newThread, first_post_content: first_post_content})) {
+        for (const [key, value] of Object.entries({ ...newThread, first_post_content: first_post_content })) {
             if (value == null) {
                 return res.status(400).json({
                     error: { message: `Missing '${key}' in request body` }
                 })
             }
         }
-        
+
         const threadNameError = ThreadsService.validateThreadName(newThread.name)
 
         if (threadNameError)
-          return res.status(400).json({ error: threadNameError })
+            return res.status(400).json({ error: threadNameError })
 
         if (first_post_content.trim().length == 0)
-          return res.status(400).json({ error: "First post content missing" })
+            return res.status(400).json({ error: "First post content missing" })
 
-        ThreadsService.insertThread(
+        BoardsService.getById(
             req.app.get('db'),
-            newThread,
-            first_post_content
+            req.body.board_id
         )
-            .then(thread => {
-                res
-                    .location(path.posix.join(req.originalUrl, `/${thread.id}`))
-                    .status(201)
-                    .json(serializeThread(thread))
+            .then(board => {
+                if (!board) {
+                    return res.status(404).json({ error: `Board doesn't exist` })
+                }
+
+                ThreadsService.insertThread(
+                    req.app.get('db'),
+                    newThread,
+                    first_post_content
+                )
+                    .then(thread => {
+                        res
+                            .location(path.posix.join(req.originalUrl, `/${thread.id}`))
+                            .status(201)
+                            .json(serializeThread(thread))
+                    })
+                    .catch(next)
             })
             .catch(next)
     })
